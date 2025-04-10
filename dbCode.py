2a36951cced2ae5b16d2e2f04bbe0d485c4731a6
Tre Montague <tre.montague@drake.edu>
2025-04-10 14:05:46 -0500

    Add user works as well as delete user

--- dbCode.py
+++ dbCode.py
@@ -3,13 +3,13 @@ import pymysql.cursors
 import creds
 import boto3
 
-# ----------------
-# Section 1: MySQL (RDS) Connection Helpers
-# ----------------
+# # ----------------
+# # Section 1: MySQL (RDS) Connection Helpers
+# # ----------------
 
-def get_conn():
-    """
-    Established and return a connection to the MySQL RDS database
-    using credentials from creds.py
-    Uses DictCursor to return query results as dictionaries.
-    """
+# def get_conn():
+#     """
+#     Established and return a connection to the MySQL RDS database
+#     using credentials from creds.py
+#     Uses DictCursor to return query results as dictionaries.
+#     """
